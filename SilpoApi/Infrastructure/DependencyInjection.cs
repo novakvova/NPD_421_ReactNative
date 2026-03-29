fc060a138b6820fd4f400499bd2df069a9374fe2
novakvova <novakvova@gmail.com>
2026-03-29 16:22:41 +0300

    Seed data is good

--- SilpoApi/Infrastructure/DependencyInjection.cs
+++ SilpoApi/Infrastructure/DependencyInjection.cs
@@ -1,7 +1,13 @@
-﻿using Domain;
+﻿using Application.Interfaces;
+using Application.Services;
+using Domain;
+using Domain.Entities.Identity;
+using Infrastructure.Jobs;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Quartz;
 
 namespace Infrastructure;
 
@@ -17,6 +23,32 @@ public static class DependencyInjection
 
         services.AddDbContext<AppDbContext>(options =>
             options.UseNpgsql(connectionString));
+
+        services.AddIdentity<UserEntity, RoleEntity>(options =>
+            {
+                options.Password.RequiredLength = 6;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.AddScoped<ISeederService, SeederService>();
+
+        // Quartz
+        services.AddQuartz(q =>
+        {
+            var jobKey = new JobKey("SeederJob");
+            q.AddJob<SeederJob>(opts => opts.WithIdentity(jobKey));
+            q.AddTrigger(opts => opts
+                .ForJob(jobKey)
+                .WithIdentity("SeederJob-trigger")
+                .StartNow());
+        });
+
+        services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
 
         return services;
     }
