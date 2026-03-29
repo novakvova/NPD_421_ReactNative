ad43fbd74cf5df6c95f943a8e4cd534098a57300
novakvova <novakvova@gmail.com>
2026-03-29 14:05:47 +0300

    working expo go

---
+++ SilpoApi/SilpoApi/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.MapOpenApi();
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    options.OAuthUsePkce();
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
